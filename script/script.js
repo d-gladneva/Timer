876968dd794216c4ff689da8a85fe8da4f73ae22
Dasha <davydova_di@mail.ru>
2020-09-20 13:05:15 +0300

    feat: animation popup|script.js

--- script/script.js
+++ script/script.js
@@ -20,19 +20,19 @@ window.addEventListener('DOMContentLoaded', () => {
             let timer = getTimeRemaining();
 
             timerHours.textContent = timer.hours;
-            if(timer.hours < 10) {
-                timerHours.textContent = '0'+timer.hours;
+            if (timer.hours < 10) {
+                timerHours.textContent = '0' + timer.hours;
             }
             timerMinutes.textContent = timer.minutes;
-            if(timer.minutes < 10) {
-                timerMinutes.textContent = '0'+timer.minutes;
+            if (timer.minutes < 10) {
+                timerMinutes.textContent = '0' + timer.minutes;
             }
             timerSeconds.textContent = timer.seconds;
-            if(timer.seconds < 10) {
-                timerSeconds.textContent = '0'+timer.seconds;
+            if (timer.seconds < 10) {
+                timerSeconds.textContent = '0' + timer.seconds;
             }
             if (timer.timeRemaining > 0) {
-             let timeOut = setTimeout(upDateClock, 1000);
+                let timeOut = setTimeout(upDateClock, 1000);
             } else {
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
@@ -46,4 +46,77 @@ window.addEventListener('DOMContentLoaded', () => {
 
     countTimer('01 october 2020');
     // let calcDate = setInterval(countTimer, 1000, '01 october 2020');
+
+    // меню
+    const menu = document.querySelector('menu');
+    const menuItems = menu.querySelectorAll('ul>li');
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu');
+        const closeBtn = document.querySelector('.close-btn');
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+
+    };
+    toggleMenu();
+
+    // popup
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup');
+        const popupBtn = document.querySelectorAll('.popup-btn');
+        const popupClose = document.querySelectorAll('.popup-close');
+        const popupContent = document.querySelector('.popup-content');
+        let count = 0;
+        let animateInterval;
+        popupBtn.forEach(elem => elem.addEventListener('click', () => {
+            popup.style.display = 'block';
+        }));
+
+        popupClose.forEach(elem => elem.addEventListener('click', () => {
+            popup.style.display = 'none';
+        }));
+        let animatePopUp = () => {
+            animateInterval = requestAnimationFrame(animatePopUp);
+            count++;
+            if (document.documentElement.clientWidth > 768){
+                if (count < 250) {
+                    popupContent.style.top = count + 'px';
+                } else cancelAnimationFrame(animateInterval)
+            } else cancelAnimationFrame(animateInterval);
+        };
+        animateInterval = requestAnimationFrame(animatePopUp);
+    };
+
+    togglePopUp();
+
+    // scroll
+
+    const getPosition = (elem) => {
+        return document.documentElement.scrollTop + elem.getBoundingClientRect().y;
+    };
+
+    const scrollToTarget = (e) => {
+            const targetId = e.target.getAttribute('to');
+            console.log(targetId);
+            const target = document.getElementById(targetId.substring(1,targetId.length));
+            console.log(target);
+            const targetPosition = getPosition(target);
+            const offsetTargetPosition = targetPosition - 100;
+
+            window.scrollTo({
+                top: offsetTargetPosition,
+                behavior: 'smooth'
+            });
+    };
+
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', scrollToTarget);
+    }
+
 });
