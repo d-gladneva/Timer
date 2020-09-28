a37861e1586a981bf357cf938aeec6711746df1e
Dasha <davydova_di@mail.ru>
2020-09-28 19:58:21 +0300

    feat: function runNumberd|script.js

--- script/script.js
+++ script/script.js
@@ -360,4 +360,75 @@ window.addEventListener('DOMContentLoaded', () => {
 
     slider();
 
+    // калькулятор
+
+    const calc = (price = 100) => {
+        const calcType = document.querySelector('.calc-type');
+        const calcSquare = document.querySelector('.calc-square');
+        const calcDay = document.querySelector('.calc-day');
+        const calcCount = document.querySelector('.calc-count');
+        const totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0;
+            console.log(typeof total);
+            let countValue = 1;
+            let dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = +Math.round(price * typeValue * squareValue * countValue * dayValue);
+            }
+
+            totalValue.textContent = total;
+
+            const runNumbers = (val, timeout) => {
+                let count = 0;
+
+                let interval = setInterval(()=> {
+                    if (count < val) {
+                        count = count + 1;
+                        if (calcSquare.value !== '' || calcDay.value !== '' || calcCount.value !== ''){
+                            clearInterval(interval);
+                            totalValue.textContent = total;
+                        } else {
+                            totalValue.textContent = count;
+                        }
+                    } else {
+                        count = 0;
+                        count = count + 1;
+                        totalValue.textContent = count;
+                    }}, timeout)
+
+            };
+
+            runNumbers(100, 40);
+
+        };
+
+
+
+        calcBlock.addEventListener('change', (e) => {
+            const target = e.target;
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+
+
+    };
+
+    calc(100);
+
 });
